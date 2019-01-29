697c4d1114b030f1032f927e07ae7109fa18173b
tom <1682999@qq.com>
2019-01-29 19:50:00 +0800

    第二次添加

---
+++ xaiomi/src/main/java/com/example/xaiomi/XaiomiApplication.java
@@ -0,0 +1,14 @@
+package com.example.xaiomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaiomiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XaiomiApplication.class, args);
+    }
+
+}
+
