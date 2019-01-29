b054074f0da8dfa9e6fb898523dc9119c240d591
tom <1682999@qq.com>
2019-01-29 19:27:56 +0800

    第一次添加

---
+++ demodddd/src/main/java/com/example/demoddddddddd/DemoddddApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoddddddddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoddddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoddddApplication.class, args);
+    }
+
+}
+
